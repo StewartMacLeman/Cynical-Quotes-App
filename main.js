010f7e2e2d2808b362c184cb211a213ec3ec7dd9
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-09-16 23:00:22 +0100

    Main layout and styling works.

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
 "use strict";
- console.log("This is a test!");
+// Global variables. --------------------------------------------------
+let editButtonsClicked = document.querySelectorAll(".edit");
+let modalCoverDiv = document.querySelector(".modelCover");
+let editModalDiv = document.getElementById("editModalId");
+let deleteModalDiv = document.getElementById("deleteModalId");
+let editModalQuoter = document.getElementById("quoterEdit");
+let editModalQuote = document.getElementById("quoteEdit");
+
+let deleteButtonsClicked = document.querySelectorAll(".delete");
+
+// The edit model. -----------------------------------------------------
+for (let i=0; i < editButtonsClicked.length; i++){
+  editButtonsClicked[i].addEventListener("click", displayEditModal)
+};
+
+function displayEditModal(e){
+
+  let quoterText = e.target.parentElement.previousElementSibling.querySelector(".italics").textContent;
+  let quoteText = e.target.parentElement.previousElementSibling.querySelector(".bold").textContent;
+  // console.log(quoterText);
+  // console.log(quoteText);
+
+  if (editModalDiv.classList.contains("hide") == true){
+    modalCoverDiv.classList.remove("hide");
+    editModalDiv.classList.remove("hide");
+    editModalQuoter.value = quoterText;
+    editModalQuote.value = quoteText;
+  } else {
+    editModalDiv.classList.add("hide");
+  }
+}
+// The delete model. -----------------------------------------------------
+for (let i=0; i < deleteButtonsClicked.length; i++){
+  deleteButtonsClicked[i].addEventListener("click", displayDeleteModal)
+};
+
+function displayDeleteModal(e){
+  if (deleteModalDiv.classList.contains("hide") == true){
+    modalCoverDiv.classList.remove("hide");
+    deleteModalDiv.classList.remove("hide");
+  } else {
+    editModalDiv.classList.add("hide");
+  }
+}
